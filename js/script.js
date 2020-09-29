603f91f683d25ca83c14eb317a80b600a5658b60
Elena281083 <elena281083@mail.ru>
2020-09-29 11:09:00 +0300

    3 files modified

--- js/script.js
+++ js/script.js
@@ -2,13 +2,15 @@ document.addEventListener("DOMContentLoaded", ready);
 function ready(){
   let burger = document.getElementsByClassName('burger');
   let menu = document.getElementsByClassName('menu');
+  let link_main = document.getElementsByClassName('link_main');
+  let welcome = document.getElementsById('welcome');
   burger[0].onclick= function(event){
     event.preventDefault();
     console.log('test');
     this.classList.toggle('burger_cross'); 
     menu[0].classList.toggle('menu_show'); 
-
+link_main[0].onclick= function () {
+  consol.log ('welcome')
   }
- 
 
 }
